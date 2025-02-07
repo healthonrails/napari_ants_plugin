dc5c632905fa3b5fc92f632e0651f50baa6d59ee
healthonrails <healthonrails@gmail.com>
2025-02-06 19:08:13 -0500

    Update setup.cfg

--- napari_ants_plugin/main.py
+++ napari_ants_plugin/main.py
@@ -1,9 +1,15 @@
 from napari.plugins import plugin_manager
-from .gui.widgets import image_alignment_widget, image_transformation_widget
+from .gui.widgets import (image_alignment_widget, 
+                          image_transformation_widget,
+                          scoring_widget,
+                          run_countgd_widget
+                          )
 
 # Register the widgets with Napari
 def napari_experimental_provide_dock_widget():
     return [
         image_alignment_widget,
         image_transformation_widget,
+        scoring_widget,
+        run_countgd_widget
     ]
