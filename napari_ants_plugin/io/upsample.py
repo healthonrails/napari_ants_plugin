fix: use np.ceil for full_target_shape computation in cube upsampling
Previously, using round() for calculating the full target shape of upsampled cubes
could result in dimensions that were too small, leading to broadcasting errors during
writes to the Zarr array. This commit replaces round() with np.ceil() to ensure that
each upsampled cube is large enough to be cropped to the desired dimensions.

napari_ants_plugin/io/upsample.py
@@ -221,9 +221,9 @@ class AtlasUpsampler:
             f"Created final Zarr dataset at '{self.final_zarr_path}' with chunk shape {chunks}.")
 
         full_target_shape = (
-            int(round(cube_size[0] * new_zoom_factors[0])),
-            int(round(cube_size[1] * new_zoom_factors[1])),
-            int(round(cube_size[2] * new_zoom_factors[2]))
+            int(np.ceil(cube_size[0] * new_zoom_factors[0])),
+            int(np.ceil(cube_size[1] * new_zoom_factors[1])),
+            int(np.ceil(cube_size[2] * new_zoom_factors[2]))
         )
 
         self.write_queue = queue.Queue()